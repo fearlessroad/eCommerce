c50c3425e8fef8a3ec39a00f3b0a76531bab45c9
Abigail <acdommer@gmail.com>
2016-07-27 16:33:51 -0700

    SOMEONE TOUCHED THE ROUTES FILE

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,9 +4,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'Products';
 $route['404_override'] = '';
-<<<<<<< HEAD
-
-=======
->>>>>>> dea0d4fe3428da2339a1fb71d3d5d7a96e75229a
 $route['translate_uri_dashes'] = FALSE;
 $route['adminLogin'] = 'dashboards/login';
