f173415b1b3549f9d1c3f425b92eadeeca1fe663
Chris Bautista <c.bautista.626@gmail.com>
2016-07-27 14:51:29 -0700

    group changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,13 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-<<<<<<< HEAD
 
 $route['default_controller'] = 'Products';
-
 $route['404_override'] = '';
-=======
-$route['default_controller'] = 'users';
-$route['404_override'] = 'users';
->>>>>>> 2c33a11074b864d91190c78fe9a3aaec1f0df89c
 $route['translate_uri_dashes'] = FALSE;
